8faf54f696063594c88ae04b1bc5897f92f4e9f9
xu.ni.hao@163.com <xu.ni.hao@163.com>
2021-12-26 21:40:16 +0800

    11

--- antdProV5/src/app.tsx
+++ antdProV5/src/app.tsx
@@ -48,58 +48,6 @@ export async function getInitialState(): Promise<{
   };
 }
 
-/**
- * 异常处理程序
-    200: '服务器成功返回请求的数据。',
-    201: '新建或修改数据成功。',
-    202: '一个请求已经进入后台排队（异步任务）。',
-    204: '删除数据成功。',
-    400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
-    401: '用户没有权限（令牌、用户名、密码错误）。',
-    403: '用户得到授权，但是访问是被禁止的。',
-    404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
-    405: '请求方法不被允许。',
-    406: '请求的格式不可得。',
-    410: '请求的资源被永久删除，且不会再得到的。',
-    422: '当创建一个对象时，发生一个验证错误。',
-    500: '服务器发生错误，请检查服务器。',
-    502: '网关错误。',
-    503: '服务不可用，服务器暂时过载或维护。',
-    504: '网关超时。',
- //-----English
-    200: The server successfully returned the requested data. ',
-    201: New or modified data is successful. ',
-    202: A request has entered the background queue (asynchronous task). ',
-    204: Data deleted successfully. ',
-    400: 'There was an error in the request sent, and the server did not create or modify data. ',
-    401: The user does not have permission (token, username, password error). ',
-    403: The user is authorized, but access is forbidden. ',
-    404: The request sent was for a record that did not exist. ',
-    405: The request method is not allowed. ',
-    406: The requested format is not available. ',
-    410':
-        'The requested resource is permanently deleted and will no longer be available. ',
-    422: When creating an object, a validation error occurred. ',
-    500: An error occurred on the server, please check the server. ',
-    502: Gateway error. ',
-    503: The service is unavailable. ',
-    504: The gateway timed out. ',
- * @see https://beta-pro.ant.design/docs/request-cn
- */
-export const request: RequestConfig = {
-  errorHandler: (error: any) => {
-    const { response } = error;
-
-    if (!response) {
-      notification.error({
-        description: '您的网络发生异常，无法连接服务器',
-        message: '网络异常',
-      });
-    }
-    throw error;
-  },
-};
-
 // ProLayout 支持的api https://procomponents.ant.design/components/layout
 export const layout: RunTimeLayoutConfig = ({ initialState }) => {
   return {
@@ -131,6 +79,75 @@ export const layout: RunTimeLayoutConfig = ({ initialState }) => {
     menuHeaderRender: undefined,
     // 自定义 403 页面
     // unAccessible: <div>unAccessible</div>,
+    // 增加一个 loading 的状态
+    // childrenRender: (children) => {
+    //   if (initialState.loading) return <PageLoading />;
+    //   return children;
+    // },
     ...initialState?.settings,
   };
+};
+
+const codeMessage = {
+  200: '服务器成功返回请求的数据。',
+  201: '新建或修改数据成功。',
+  202: '一个请求已经进入后台排队（异步任务）。',
+  204: '删除数据成功。',
+  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
+  401: '用户没有权限（令牌、用户名、密码错误）。',
+  403: '用户得到授权，但是访问是被禁止的。',
+  404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
+  405: '请求方法不被允许。',
+  406: '请求的格式不可得。',
+  410: '请求的资源被永久删除，且不会再得到的。',
+  422: '当创建一个对象时，发生一个验证错误。',
+  500: '服务器发生错误，请检查服务器。',
+  502: '网关错误。',
+  503: '服务不可用，服务器暂时过载或维护。',
+  504: '网关超时。',
+};
+
+/**
+ * 异常处理程序
+ */
+const errorHandler = (error: any) => {
+  const { response } = error;
+  console.log(response);
+  if (response && response.status !== 200 && response.status !== 422) {
+    const errorText = codeMessage[response.status] || response.statusText;
+    const { status, url } = response;
+
+    if (response.status === 401 || response.status === 403) {
+      notification.error({
+        message: `认证超时或者禁示访问`,
+        description: '认证超时或者禁示访问，请重新登录',
+      });
+      history.push('/user/login');
+    } else {
+      notification.error({
+        message: `请求错误 ${status}: ${url}`,
+        description: errorText,
+      });
+    }
+  }
+
+  if (!response) {
+    notification.error({
+      description: '您的网络发生异常，无法连接服务器',
+      message: '网络异常',
+    });
+  }
+  throw error;
+};
+
+// const DOMAIN =
+//   process.env.NODE_ENV === 'production' ? `http://staging.qiuzhi99.com` : `http://localhost:6060`;
+const DOMAIN = process.env.REQUST_BASE_URL;
+
+export const request: RequestConfig = {
+  errorHandler,
+  prefix: `${DOMAIN}/api`,
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem('token')}`,
+  },
 };
